afb44310a25762476e398e29ad0ad539bf298948
12Mahmud99 <106562348+12Mahmud99@users.noreply.github.com>
2022-06-17 00:49:53 -0700

    Update searcher.java

--- searcher.java
+++ searcher.java
@@ -9,13 +9,21 @@ public class MyClass {
         Scanner scnr = new Scanner(System.in);
         String file = scnr.nextLine();
         Scanner input = new Scanner(Paths.get(file));
+        int u =0;
         String s;
-        char c = scnr.next().charAt(0);
-        String[] arr2 = new String[4000];
+        while(input.hasNextLine()){
+            s= input.nextLine();
+            ++u;
+        }
+        System.out.print(u);
+        char c = scnr.nextLine().charAt(0);
+        String[] arr2 = new String[u];
         int i =0;
         for(int j =0; j <arr2.length; ++j){
-        
+        if(input.hasNextLine()){
         arr2[j] = input.nextLine();
+            
+        }
         }
         for
         (i = 0; i<arr2.length; ++i){
@@ -23,6 +31,8 @@ public class MyClass {
             System.out.println(arr2[i]);
         }
         
+        
             
         }
     }
+
